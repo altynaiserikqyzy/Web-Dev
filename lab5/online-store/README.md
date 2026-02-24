b72929e37e4e1c56bc532adc1bc52e15ad8cb1c6
altynaiserikqyzy <altynaiserikqyzyy@gmail.com>
2026-02-24 22:28:33 +0500

    lab5

---
+++ lab5/online-store/README.md
@@ -0,0 +1,25 @@
+# Online Store - Lab5
+
+Angular project simulating an online store with categories and products.
+
+## Project Structure
+## How to Run
+
+git clone https://github.com/altynaiserikqyzy/Web-Dev.git
+cd lab5/online-store
+npm install
+ng serve
+
+
+Features
+	- Display products by category
+    - Image gallery with main image and thumbnails
+    - Like button to increment product likes
+    - Delete button to remove products from the list
+    - Share buttons for WhatsApp and Telegram
+    - Responsive product cards with hover effects
+    - Empty category message: “No products in this category”
+Notes
+	•	Do not push the node_modules/ folder. It is already included in .gitignore.
+	•	All products are linked to real items from Kaspi.kz.
+	•	This project uses Angular standalone components (standalone: true) for ProductList and ProductItem.
---
+++ lab5/online-store/src/app/AppComponent/app.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { Category } from '../models/category.models';
+import { Product } from '../models/product.model';
+import { ProductService } from '../services/ProductService';
+import { ProductListComponent } from '../components/product-list/product-list.component';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.html',
+  styleUrls: ['./app.css'],
+  imports: [ProductListComponent] 
+})
+export class AppComponent {
+  categories : Category[]= [];
+  selectedCategoryID : number | null = null;
+  selectedProducts: Product[]= [];
+  constructor(private productService : ProductService ){
+  this.categories= this.productService.getCategories();
+  }
+
+  selectCategory(categoryID:number){
+    this.selectedCategoryID = categoryID;
+    this.selectedProducts = this.productService.getProductByCategories(categoryID);
+  }
+  mainPage(){
+    this.selectedCategoryID=null;
+    this.selectedProducts=[];
+
+  }
+
+
+}
